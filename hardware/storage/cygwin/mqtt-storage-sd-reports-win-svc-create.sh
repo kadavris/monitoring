c2ae72068206324fa1a26b59bdb633299c949138
Andrii Pakhutin <pakhutin@gmail.com>
2026-02-11 22:42:55 +0200

    Installation script and source names fixes

--- hardware/storage/cygwin/mqtt-storage-sd-reports-win-svc-create.sh
+++ hardware/storage/cygwin/mqtt-storage-sd-reports-win-svc-create.sh
@@ -2,22 +2,22 @@
 # This script is used to create a Windows service for running reporter in the background
 # CygWin is assumed to be installed
 svcname='mqtt-storage-sd-reports'
-looptime=60  # how amny secs to put in the --loop
+looptime=60  # how many secs to put in the --loop
 
 if [ "$1" == "" ]
 then
-  echo 'Use with <path to log folder> argument'
-  echo Run it from the folder where reporter scripts live
-  echo example:
-  echo $0 \"/cygdrive/d/log\"
-  echo Or if you''re using non-cygwin python install, use windows path type:
-  exit
+    echo 'Use with <path to log folder> argument'
+    echo Run it from the folder where reporter scripts live
+    echo example:
+    echo $0 \"/cygdrive/d/log\"
+    echo Or if you''re using non-cygwin python install, use windows path type:
+    exit
 fi
 
 logdir="$1"
 if [ ! -d "$1" ]; then
-  echo log dir: $1 is not a directory!
-  exit 1
+    echo log dir: $1 is not a directory!
+    exit 1
 fi
 
 cwd=`pwd`
@@ -27,32 +27,32 @@ cwd=`pwd`
 pyexe=`which python3 2>/dev/null | head -1`
 
 if [ "$pyexe" == "" ]; then
-  # fallback to whatever will be
-  pyexe=`which python | head -1`
+    # fallback to whatever will be
+    pyexe=`which python | head -1`
 fi
 
 # resolve links if any
 while [ -L "$pyexe" ]
 do
-  pyexe=`readlink "$pyexe"`
+    pyexe=`readlink "$pyexe"`
 done
 
 # not a full path yet?
 if [ ! -f $pyexe ]
 then
-  pyexe=`which "$pyexe" | head -1`
+    pyexe=`which "$pyexe" | head -1`
 fi
 
 # querying python type
 # Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)]
 pver=`python -V -V`
 if [[ $pver =~ 'cygwin' ]]; then
-  ptype='cygwin'
-  svccmd="$cwd/$svcname -l $looptime"
+    ptype='cygwin'
+    svccmd="$cwd/$svcname -l $looptime"
 else
-  ptype='native'
-  cwd=`/usr/bin/cygpath -w "$cwd/"`
-  svccmd="\"${cwd}$svcname\" -l $looptime -c \"$cwd\\reporters.ini\""
+    ptype='native'
+    cwd=$(/usr/bin/cygpath -w "$cwd/")
+    svccmd="\"${cwd}$svcname\" -l $looptime -c \"$cwd\\reporters.ini\""
 fi
 
 echo 'Stopping.'
@@ -63,5 +63,5 @@ echo 'Installing:'
 echo "$ptype python found at '$pyexe'"
 /usr/bin/cygrunsrv -V -I $svcname -p "${pyexe}" -a "$svccmd" -1 "$logdir/$svcname.log" -2 "$logdir/$svcname.log"
 /usr/bin/cygrunsrv -V -Q $svcname
-echo 'Running.'
-/usr/bin/cygrunsrv -V -S $svcname
+#echo 'Running.'
+#/usr/bin/cygrunsrv -V -S $svcname
--- install.sh
+++ install.sh
@@ -1,34 +1,57 @@
 #!/bin/bash
-# This is an install script for kadavris/monitoring repo
+# This is the install script for kadavris/monitoring repo
 # Run without arguments to get help
 
-EXEDEST="/usr/local/lib/kadavris/monitoring"
+ENVFILE="./service-env"
+
+if [ "$1" == "" ]; then
+    echo "Use 'install.sh [-e <env file name>] <name> ... <name>'"
+    echo "  '-e' option is to provide the existing environment variable list file with correct paths"
+    echo "     by default $ENVFILE will be used"
+    echo "For '<name>' - specify 'all' to install all of the utilities from this repo,"
+    echo "or a combination of 'mikrotik', 'power' and 'storage' to install specific ones."
+    echo "NOTE: This script will not overwrite existing .ini files."
+    exit 0
+fi
+
 SYSTEMD="/etc/systemd/system"
-ETC="/etc/smarthome/monitoring"
 
 USER="smarthome"
 GROUP="smarthome"
+
 /usr/bin/id "$USER" 2> /dev/null
 if [ $? == 1 ]; then
-    if [ -d "$ETC" ]; then
-        USER=`/usr/bin/stat --printf %U "$ETC"`
-        GROUP=`/usr/bin/stat --printf %G "$ETC"`
-        echo I see that you use $USER:$GROUP credentials. Will obey.
+    if [ -d "$CONFDIR" ]; then
+        USER=$(/usr/bin/stat --printf %U "$CONFDIR")
+        GROUP=$(/usr/bin/stat --printf %G "$CONFDIR")
+        echo "I see that you use $USER:$GROUP credentials. Will obey."
     else
-        echo ERROR! Cannot determine what UID/GID to use.
-        echo You may want to add these by running:
-        echo useradd -d \"$ETC\" -s /sbin/nologin -g $GROUP $USER
+        echo "ERROR! Cannot determine what UID/GID to use."
+        echo "You may want to add these by running:"
+        echo "useradd -d '$CONFDIR' -s /sbin/nologin -g $GROUP $USER"
         exit 1
     fi
 fi
 
 OWNER="--owner=${USER} --group=${GROUP}"
 
-EXEOPT="-D ${OWNER} --mode=0755"
-SVCOPT="${OWNER} --mode=0644"
-INIOPT="-D ${OWNER} --mode=0640"
+EXEOPT="-v -C -D ${OWNER} --mode=0755"
+SVCOPT="-v -C ${OWNER} --mode=0644"
+INIOPT="-v -C -D ${OWNER} --mode=0640"
 
 INST="/usr/bin/install"
+
+while getopts "e:" OPT; do
+    if [ "$OPT" = "e" ]; then
+        ENVFILE=$OPTARG
+    fi
+done
+
+# We'll use BINDIR, CONFDIR and PYTHONPATH from here:
+. $ENVFILE
+
+# python's import dest
+py_namespace="$PYTHONPATH/kadpy"
 
 # in: <src file> <dst dir> <new name or ''> <install params>
 function install_to_dir_w_check() {
@@ -48,47 +71,52 @@ function install_to_dir_w_check() {
     fi
 }
 
+function install_deps() {
+    install_to_dir_w_check "service-env" "$CONFDIR" "service-env" "$INIOPT"
+    $INST $EXEOPT -t "$py_namespace" "imports/__init__.py"
+    $INST $EXEOPT -t "$py_namespace" "imports/kmqtt.py"
+    if [ -z "$1" ]; then
+        return
+    fi
+    for d in $*; do
+        $INST $EXEOPT -t "$py_namespace" "imports/$d"
+    done
+}
+
 function install_mikrotik() {
-    ETCMIK="${ETC}/mikrotik"
+    install_deps
     srcd="hardware/mikrotik"
-    $INST $EXEOPT "${srcd}/mikrotik2mqtt" "$EXEDEST"
+    $INST $EXEOPT "${srcd}/mikrotik2mqtt" "$BINDIR"
     install_to_dir_w_check "${srcd}/mikrotik2mqtt.service.sample" "${SYSTEMD}" "mikrotik2mqtt.service" "$SVCOPT"
-    install_to_dir_w_check "${srcd}/mikrotik2mqtt.ini.sample" "$ETCMIK" "mikrotik2mqtt.ini" "$INIOPT"
+    install_to_dir_w_check "${srcd}/mikrotik2mqtt.ini.sample" "$CONFDIR" "mikrotik2mqtt.ini" "$INIOPT"
 }
 
 function install_power() {
-    ETCPOW="${ETC}/power"
+    install_deps kbattstats.py kbattlead.py
     srcd="hardware/power"
-    $INST $EXEOPT "${srcd}/mqtt-power" "$EXEDEST"
+    $INST $EXEOPT "${srcd}/mqtt-power" "$BINDIR"
     install_to_dir_w_check "${srcd}/mqtt-power.service.sample" "${SYSTEMD}" "mqtt-power.service" "$SVCOPT"
-    install_to_dir_w_check "${srcd}/mqtt-power.ini_sample" "$ETCPOW" "mqtt-power.ini" "$INIOPT"
+    install_to_dir_w_check "${srcd}/mqtt-power.sample.ini" "$CONFDIR" "mqtt-power.ini" "$INIOPT"
 }
 
 function install_storage() {
-    ETCSTOR="${ETC}/storage"
+    install_deps
     srcd="hardware/storage"
-    $INST $EXEOPT "${srcd}/mqtt-storage-sd-reports" "$EXEDEST"
+    $INST $EXEOPT "${srcd}/mqtt-storage-sd-reports" "$BINDIR"
     install_to_dir_w_check "${srcd}/mqtt-storage-sd-reports.service.sample" "${SYSTEMD}" "mqtt-storage-sd-reports.service" "$SVCOPT"
-    install_to_dir_w_check "${srcd}/mqtt-storage.ini_sample" "$ETCSTOR" "mqtt-storage.ini" "$INIOPT"
+    install_to_dir_w_check "${srcd}/mqtt-storage.sample.ini" "$CONFDIR" "mqtt-storage.ini" "$INIOPT"
 }
 
 ########################
-if [ "$1" == "" ]; then
-    echo Use \'install.sh all\' to process all of the files
-    echo Or specify \'mikrotik\' or \'power\' or \'storage\' to install specific ones
-    echo NOTE: This script will not overwrite existing .ini files.
-    exit 0
-fi
-
-if [ ! -d "$ETC" ]; then
-    /usr/bin/mkdir -p -m 0770 "$ETC"
-    /usr/bin/chown "$USER:$GROUP" "$ETC"
-fi
-
 if [ "$1" == "all" ]; then
     list="mikrotik power storage"
 else
     list="$1"
+fi
+
+if [ ! -d "$CONFDIR" ]; then
+    /usr/bin/mkdir -p -m 0770 "$CONFDIR"
+    /usr/bin/chown "$USER:$GROUP" "$CONFDIR"
 fi
 
 for opt in $list
@@ -97,6 +125,7 @@ do
 done
 
 echo "You may need to check the permissons and ownership of intermediate directories:"
-echo "  - $ETC"
-echo "  - $EXEDEST"
-echo "Due to 'install' utility not making it all the way up"
+echo "  - $CONFDIR"
+echo "  - $BINDIR"
+echo "  - $PYTHONPATH"
+echo "... due to 'install' utility not making it all the way up"
